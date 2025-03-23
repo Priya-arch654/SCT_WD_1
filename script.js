a783eeaf3e2bb7d3180f49cace44c3e09258eb1e
HARI PRIYA A <aharipriya019@gmail.com>
2025-03-23 13:05:07 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Simple script to handle the "Get Started" button click
+document.getElementById("cta-btn").addEventListener("click", function() {
+    alert("Thank you for getting started!");
+});
